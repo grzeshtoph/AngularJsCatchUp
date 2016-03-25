f67ebc7dc25894818db998f0fdf658532f4d4414
greg <greg.smorag@gmail.com>
2016-03-25 10:17:38 +0000

    First commit

---
+++ src/main/java/com/greg/angularjs/AngularJsCatchUpApplication.java
@@ -0,0 +1,12 @@
+package com.greg.angularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJsCatchUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJsCatchUpApplication.class, args);
+	}
+}
